69d3f28bddbc6928d990d94d9129b59c5cc9c802
jxmichael111 <131832250+jxmichael111@users.noreply.github.com>
2023-07-21 19:25:17 -0500

    Update Model.cpp

--- Floppa war/Model.cpp
+++ Floppa war/Model.cpp
@@ -16,10 +16,3 @@ void Model::generarMapa() {
 void Model::actualizarMapa(bool direccion[4]) { 
     maparef->moverMapa(direccion);
 }
-
-bool Model::colisiones(int X, int Y, int posX, int posY, int width, int lenght) {
-    if (posX <= X && X <= posX + width && posY <= Y && Y <= posY + lenght)
-        return true;
-    else
-        return false;
-}
