5b94e1ad360c73edc37c9bd0244211cb81b587c9
Daniel Quiñones <72476805+DanielfQo@users.noreply.github.com>
2023-07-25 11:08:44 -0500

    Delete EnemigoModel.cpp

--- Floppa war/Imagenes/EnemigoModel.cpp
+++
@@ -1,9 +0,0 @@
-#include "EnemigoModel.h"
-void MeleeEnemigoModel::moverse(float x, float y){}
-void MeleeEnemigoModel::atacar() {}
-
-void RangedEnemigoModel::moverse(float x, float y) {}
-void RangedEnemigoModel::atacar() {}
-
-void TanqueEnemigoModel::moverse(float x, float y) {}
-void TanqueEnemigoModel::atacar() {}
