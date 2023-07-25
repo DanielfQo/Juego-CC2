4a8b87ab718a7e6f55362b20cda10427e41cbd72
Daniel Quiñones <72476805+DanielfQo@users.noreply.github.com>
2023-07-25 11:11:05 -0500

    Delete EntidadViewer.cpp

--- Floppa war/Imagenes/EntidadViewer.cpp
+++
@@ -1,8 +0,0 @@
-#include "EntidadViewer.h"
-
-EntidadViewer::EntidadViewer(std::string rutaImagen_) {
-    //llamamos al constructor de Imagen con el nombre de archivo proporcionado
-    rutaImagen = rutaImagen_;
-    spriteTexture.loadFromFile(rutaImagen);
-    sprite.setTexture(spriteTexture);
-}
